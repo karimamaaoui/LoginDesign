d159103bd6cdc7d297c7d71e4a832c085965d5b3
karimamaaoui <maaouika07@gmail.com>
2024-04-08 00:32:28 +0100

    Initial Commit

---
+++ AuthDesign/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  AuthDesign
+//
+//  Created by karimamaaoui on 4/8/24.
+//  Copyright © 2024 karimamaaoui. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
